ff03aee1054a383d4e28eb1bebd5cece3391c238
Kirsten Greed <kirsteng@jobtalk.com.au>
2020-06-20 05:16:03 +1000

    drag drop between gallery controls

--- WindowsFormsGallery/Form2.Designer.cs
+++ WindowsFormsGallery/Form2.Designer.cs
@@ -28,20 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup1 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem1 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem2 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup2 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+            DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup3 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem3 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem4 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup4 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem5 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem6 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem7 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup5 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem8 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            DevExpress.XtraBars.Ribbon.GalleryItem galleryItem9 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+            this.galleryDropDown1 = new DevExpress.XtraBars.Ribbon.GalleryDropDown(this.components);
+            this.galleryControl2 = new DevExpress.XtraBars.Ribbon.GalleryControl();
+            this.galleryControlClient2 = new DevExpress.XtraBars.Ribbon.GalleryControlClient();
+            this.galleryControl1 = new DevExpress.XtraBars.Ribbon.GalleryControl();
+            this.galleryControlClient1 = new DevExpress.XtraBars.Ribbon.GalleryControlClient();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryDropDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryControl2)).BeginInit();
+            this.galleryControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).BeginInit();
+            this.galleryControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // galleryDropDown1
+            // 
+            this.galleryDropDown1.Manager = null;
+            this.galleryDropDown1.Name = "galleryDropDown1";
+            // 
+            // galleryControl2
+            // 
+            this.galleryControl2.AllowDrop = true;
+            this.galleryControl2.Controls.Add(this.galleryControlClient2);
+            // 
+            // 
+            // 
+            this.galleryControl2.Gallery.AllowItemDrag = true;
+            this.galleryControl2.Gallery.CheckSelectedItemViaKeyboard = true;
+            galleryItemGroup1.Caption = "Group3";
+            galleryItem1.Caption = "Item6";
+            galleryItem2.Caption = "Item7";
+            galleryItemGroup1.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem1,
+            galleryItem2});
+            galleryItemGroup2.Caption = "Group4";
+            galleryItemGroup3.Caption = "Group5";
+            galleryItem3.Caption = "Item8";
+            galleryItem4.Caption = "Item9";
+            galleryItemGroup3.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem3,
+            galleryItem4});
+            this.galleryControl2.Gallery.Groups.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItemGroup[] {
+            galleryItemGroup1,
+            galleryItemGroup2,
+            galleryItemGroup3});
+            this.galleryControl2.Location = new System.Drawing.Point(406, 34);
+            this.galleryControl2.Name = "galleryControl2";
+            this.galleryControl2.Size = new System.Drawing.Size(371, 389);
+            this.galleryControl2.TabIndex = 1;
+            this.galleryControl2.Text = "galleryControl2";
+            this.galleryControl2.DragDrop += new System.Windows.Forms.DragEventHandler(this.galleryControl_DragDrop);
+            this.galleryControl2.DragOver += new System.Windows.Forms.DragEventHandler(this.galleryControl_DragOver);
+            this.galleryControl2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.galleryControl_MouseDown);
+            this.galleryControl2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.galleryControl_MouseMove);
+            // 
+            // galleryControlClient2
+            // 
+            this.galleryControlClient2.GalleryControl = this.galleryControl2;
+            this.galleryControlClient2.Location = new System.Drawing.Point(2, 31);
+            this.galleryControlClient2.Size = new System.Drawing.Size(350, 356);
+            // 
+            // galleryControl1
+            // 
+            this.galleryControl1.AllowDrop = true;
+            this.galleryControl1.Controls.Add(this.galleryControlClient1);
+            // 
+            // 
+            // 
+            this.galleryControl1.Gallery.AllowItemDrag = true;
+            this.galleryControl1.Gallery.CheckSelectedItemViaKeyboard = true;
+            galleryItemGroup4.Caption = "Group1";
+            galleryItem5.Caption = "Item1";
+            galleryItem5.Checked = true;
+            galleryItem5.Description = "1";
+            galleryItem5.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            galleryItem5.Value = 0;
+            galleryItem6.Caption = "Item2";
+            galleryItem6.Value = 1;
+            galleryItem7.Caption = "Item3";
+            galleryItem7.Value = 2;
+            galleryItemGroup4.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem5,
+            galleryItem6,
+            galleryItem7});
+            galleryItemGroup5.Caption = "Group2";
+            galleryItem8.Caption = "Item4";
+            galleryItem8.Value = 3;
+            galleryItem9.Caption = "Item5";
+            galleryItem9.Value = 4;
+            galleryItemGroup5.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem8,
+            galleryItem9});
+            this.galleryControl1.Gallery.Groups.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItemGroup[] {
+            galleryItemGroup4,
+            galleryItemGroup5});
+            this.galleryControl1.Location = new System.Drawing.Point(22, 34);
+            this.galleryControl1.Name = "galleryControl1";
+            this.galleryControl1.Size = new System.Drawing.Size(368, 389);
+            this.galleryControl1.TabIndex = 0;
+            this.galleryControl1.Text = "galleryControl1";
+            this.galleryControl1.DragDrop += new System.Windows.Forms.DragEventHandler(this.galleryControl_DragDrop);
+            this.galleryControl1.DragOver += new System.Windows.Forms.DragEventHandler(this.galleryControl_DragOver);
+            this.galleryControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.galleryControl_MouseDown);
+            this.galleryControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.galleryControl_MouseMove);
+            // 
+            // galleryControlClient1
+            // 
+            this.galleryControlClient1.GalleryControl = this.galleryControl1;
+            this.galleryControlClient1.Location = new System.Drawing.Point(2, 31);
+            this.galleryControlClient1.Size = new System.Drawing.Size(347, 356);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.galleryControl2);
+            this.Controls.Add(this.galleryControl1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.galleryDropDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.galleryControl2)).EndInit();
+            this.galleryControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.galleryControl1)).EndInit();
+            this.galleryControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraBars.Ribbon.GalleryDropDown galleryDropDown1;
+        private DevExpress.XtraBars.Ribbon.GalleryControl galleryControl2;
+        private DevExpress.XtraBars.Ribbon.GalleryControlClient galleryControlClient2;
+        private DevExpress.XtraBars.Ribbon.GalleryControl galleryControl1;
+        private DevExpress.XtraBars.Ribbon.GalleryControlClient galleryControlClient1;
     }
 }
--- WindowsFormsGallery/Form2.cs
+++ WindowsFormsGallery/Form2.cs
@@ -1,57 +1,194 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.Data;
 using DevExpress.XtraBars.Ribbon;
-using DevExpress.Utils;
-using DevExpress.Utils.Drawing;
-using DevExpress.XtraBars.Ribbon.ViewInfo;
 namespace WindowsFormsGallery
 {
     public partial class Form2 : Form
     {
+        
+
         public Form2()
         {
             InitializeComponent();
+            hitPoint = pointInvalid;
         }
+
+        private Point hitPoint { get; set; }
+        private Point pointInvalid => new Point(-10000, -10000);
+        private GalleryControl sourceControl { get; set; }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            GalleryControl gc = new GalleryControl {Dock = DockStyle.Fill};
-            this.Controls.Add(gc);
+            
+        }
 
-            Image im1 = Image.FromFile("c:\\Images\\BMW.jpg");
-            Image im2 = Image.FromFile("c:\\Images\\Ford.jpg");
-            Image im3 = Image.FromFile("c:\\Images\\MercedecBenz.jpg");
-           
+        private void galleryControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            var galControl = sender as GalleryControl;
+            var hitInfo = galControl.CalcHitInfo(e.Location);
+            if (hitInfo.InGalleryItem && galControl.Gallery.GetCheckedItems().Count > 0)
+            {
+                galControl.Gallery.AllowMarqueeSelection = false;
+                hitPoint = e.Location;
+                sourceControl = galControl;
+                return;
+            }
+            galControl.Gallery.AllowMarqueeSelection = true;
+            hitPoint = pointInvalid;
+            sourceControl = null;
+        }
 
-            gc.Gallery.ItemImageLayout = ImageLayoutMode.ZoomInside;
-            gc.Gallery.ImageSize = new Size(120, 90);
-            gc.Gallery.ShowItemText = true;
-
-            GalleryItemGroup group1 = new GalleryItemGroup {Caption = "Cars"};
-            gc.Gallery.Groups.Add(group1);
+      
  
 
-            group1.Items.Add(new GalleryItem(im1, "BMW", ""));
-            group1.Items.Add(new GalleryItem(im2, "Ford", ""));
-            group1.Items.Add(new GalleryItem(im3, "Mercedec-Benz", ""));
-
-          
-            var options = gc.Gallery.ContextButtonOptions;
-            options.DisplayArea = ContextItemDisplayArea.Item;
-
-            var cb = new ContextButton
+        private void galleryControl_DragDrop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(typeof(List<GalleryItem>)))
             {
-                Name = "Move", Alignment = ContextItemAlignment.Center, Visibility = ContextItemVisibility.Visible, Caption = "Moooov"
-            };
-            gc.Gallery.ContextButtons.Add(cb);
-            gc.Gallery.RefreshGallery();
+                return;
+            }
+            var draggedItems = (List<GalleryItem>) e.Data.GetData(typeof(List<GalleryItem>)) ;
+            var galControl = sender as GalleryControl;
+            var hitInfo = galControl.CalcHitInfo(galControl.PointToClient(new Point(e.X, e.Y)));
+            if (hitInfo.InGalleryGroup)
+            {
+                foreach (var draggedItem in draggedItems)
+                {
+                    var item = draggedItem.Clone() as GalleryItem;
+                    hitInfo.GalleryItemGroup.Items.Add(item);
+                }
+            }
+            else
+            {
+                var groupIndex = galControl.Gallery.Groups.Add(new GalleryItemGroup());
+                var group = galControl.Gallery.Groups.Last();
+                group.Caption = "Dragged Items";
+                foreach (var draggedItem in draggedItems)
+                {
+                    var item = draggedItem.Clone() as GalleryItem;
+                    group.Items.Add(item);
+                }
+            }
+            //Dim draggedItems As List(Of GalleryItem) = TryCast(e.Data.GetData(GetType(List(Of GalleryItem))), List(Of GalleryItem))
+
+
+            //Dim galControl As GalleryControl = CType(sender, GalleryControl)
+
+            //Dim hitInfo As RibbonHitInfo = galControl.CalcHitInfo(galControl.PointToClient(New Point(e.X, e.Y)))
+
+            //If hitInfo.InGalleryGroup Then
+
+            //For Each draggedItem As GalleryItem In draggedItems
+
+            //hitInfo.GalleryItemGroup.Items.Add(CType(draggedItem.Clone(), GalleryItem))
+
+            //Next draggedItem
+
+            //Else
+            //    Dim groupIndex As Integer = galControl.Gallery.Groups.Add(New GalleryItemGroup())
+
+            //galControl.Gallery.Groups(groupIndex).Caption = "Dragged Items"
+
+            //For Each draggedItem As GalleryItem In draggedItems
+
+            //galControl.Gallery.Groups(groupIndex).Items.Add(CType(draggedItem.Clone(), GalleryItem))
+
+            //Next draggedItem
+
+            //End If
+        }
+
+        private void galleryControl_DragOver(object sender, DragEventArgs e)
+        {
+            var galControl = sender as GalleryControl;
+            if (galControl ==sourceControl)
+            {
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            if (e.Data.GetDataPresent(typeof(List<GalleryItem>)))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            //Dim galControl As GalleryControl = CType(sender, GalleryControl)
+
+            //If galControl Is sourceControl Then
+
+            //e.Effect = DragDropEffects.None
+
+            //Return
+            //    End If
+
+            //    If e.Data.GetDataPresent(GetType(List(Of GalleryItem))) Then
+
+            //e.Effect = DragDropEffects.Copy
+
+            //End If
+        }
+
+        private void galleryControl_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if ( e.Button != MouseButtons.Left )
+            {
+                return;
+            }
+            else
+            {
+                if (Control.ModifierKeys != Keys.None)
+                {
+                    return;
+                }
+            }
+
+            var galControl = sender as GalleryControl;
+            var checkedItems = galControl.Gallery.GetCheckedItems();
+            if (checkedItems.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                if (hitPoint == pointInvalid)
+                {
+                    return;
+                }
+            }
+            //If e.Button<> MouseButtons.Left OrElse Control.ModifierKeys<> Keys.None Then
+
+            //Return
+            //    End If
+
+            //    Dim galControl As GalleryControl = CType(sender, GalleryControl)
+
+            //Dim checkedItems As List(Of GalleryItem) = galControl.Gallery.GetCheckedItems()
+
+            //If checkedItems.Count = 0 OrElse hitPoint = pointInvalid Then
+            //    Return
+
+            //End If
+
+            var dragSize = SystemInformation.DragSize;
+            var dragRect = new Rectangle(hitPoint.X - dragSize.Width / 2, hitPoint.Y - dragSize.Height / 2, dragSize.Width, dragSize.Height);
+
+            if (dragRect.Contains(e.Location))
+            {
+                galleryControl1.DoDragDrop(checkedItems, DragDropEffects.Copy);
+            }
+            //Dim dragSize As Size = SystemInformation.DragSize
+
+            //Dim dragRect As New Rectangle(hitPoint.X - dragSize.Width \ 2, hitPoint.Y - dragSize.Height \ 2, dragSize.Width, dragSize.Height)
+
+            //If Not(dragRect.Contains(e.Location)) Then
+
+            //galleryControl1.DoDragDrop(checkedItems, DragDropEffects.Copy)
+
+            //End If
         }
     }
 }
